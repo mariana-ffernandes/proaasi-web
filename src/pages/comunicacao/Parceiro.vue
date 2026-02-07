aae42b9d85e4424d5ff7e5aca65fc65dfb2eb00e
marianafernandeslinx <mariana.fernandes@linx.com.br>
2026-02-07 19:01:08 -0300

    add base template to Parceiro page

---
+++ src/pages/comunicacao/Parceiro.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="parceiro">
+    <h1>Página Parceiro</h1>
+    <p>Conteúdo em construção.</p>
+  </section>
+</template>
+
+<script setup>
+// depois você completa
+</script>
+
+<style scoped>
+.parceiro {
+  padding: 40px;
+}
+</style>
